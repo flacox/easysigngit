833f80399fa67a4bc2d58de9df91a9c0f08bc257
LeonardoGarciaGavilan <garciagavilanleonardoant@gmail.com>
2025-02-09 16:09:42 -0400

    Actualización del nombre de usuario en el menú. Agregue un setInterval para actualizar el nombre de usuario cada 5 segundos. Ahora el menú refleja correctamente el usuario activo sin necesidad de recargar la app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FirebaseService } from './services/firebase.service';
 })
 export class AppComponent {
   @Input() nombreUsuario: string = 'Invitado';
+  private intervalId: any;
 
   public appPages = [
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
@@ -21,22 +22,40 @@ export class AppComponent {
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor(private menuController: MenuController) { }
 
-
   closeMenu() {
     this.menuController.close();
   }
+  //Se ejecuta cuando el componente se inicializa
   ngOnInit() {
-    if (!this.nombreUsuario || this.nombreUsuario === 'Invitado') {
-      const storedUser = localStorage.getItem('user');
-      if (storedUser) {
-        const user = JSON.parse(storedUser);
-        this.nombreUsuario = user.name || 'Invitado';
-      }
+    this.updateUser();
+    this.intervalId = setInterval(() => {
+      this.updateUser();
+    }, 5000); 
+  }
+// Metodo que se ejecuta cuando el componente se destruye
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
     }
   }
 
+// Metodo para actualizar el nombre del usuario
+  updateUser() {
+    //Obtengo usuario logeado
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      this.nombreUsuario = user.name || 'Invitado';
+    } else {
+      // Si no hay un usuario almacenado, asigna 'Invitado' como valor predeterminado
+      this.nombreUsuario = 'Invitado';
+    }
+  }
+
+// Inyecto el servicio de Firebase para manejar la autenticacion
   firebaseService = inject(FirebaseService);
 
+  //cerrar sesion
   signOut() {
     this.firebaseService.signOut();
   }
